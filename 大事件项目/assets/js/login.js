4733246ecd4ca586cafe6d4e0a480f9c4105bc4a
小师叔 <8225961+leijinfeng@user.noreply.gitee.com>
2020-12-29 16:16:02 +0800

    完成了登录和注册功能的开发

---
+++ 大事件项目/assets/js/login.js
@@ -0,0 +1,73 @@
+$(function () {
+    // 点击去注册账号链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+
+    })
+    // 点击去登录链接
+    $('#link_login').on('click', function () {
+        $('.reg-box').hide();
+        $('.login-box').show();
+
+    })
+
+    //从layui中获取form对象
+    var form = layui.form
+    var layer = layui.layer
+    // 通过from.verify（）函数自定义校验规则
+    form.verify({
+        // 自定义一个叫做pwd的校验字符
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        // 校验两次密码是否一致的规则
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+
+        }
+    })
+
+
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        var data = { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg('只想弱弱提示');
+            }
+            layer.msg('注册成功，请登录！');
+            // 模拟用户点击登录的行为
+            $("#link_login").click();
+        })
+    })
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！');
+                }
+                layer.msg('登录成功！')
+                // 将登录成功的token字符串保存到localstorage中
+                localStorage.setItem('token', res.token);
+                // 跳转到首页
+                location.href = 'index.html';
+
+            }
+
+        })
+    })
+
+
+})
